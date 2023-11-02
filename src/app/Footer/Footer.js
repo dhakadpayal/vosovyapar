5bf560b6c83c3c786210eaf2289777f0034ee816
SonaliMAlviya <sonaliquitustech@gmail.com>
2023-11-02 11:52:59 +0530

    f

--- src/app/Footer/Footer.js
+++ src/app/Footer/Footer.js
@@ -3,6 +3,36 @@ import React from 'react'
 export default function Footer() {
   return (
    <> 
+   <div>
+        
+          <nav className="navbar navbar-expand-lg  mynav">
+              <div className="container-fluid">
+             
+                    <Image src={vosovyapar} className='voso_logo' />
+                  
+                 <div>
+                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li className="nav-item nav_margin">
+                         <a className="nav-link active " aria-current="page" href="#">About Us</a>
+                    </li>
+                    <li className="nav-item nav_margin">
+                        <a className="nav-link active" aria-current="page" href="#">How It Work</a>
+                    </li>
+                    <li className="nav-item nav_margin">
+                        <a className="nav-link active" aria-current="page" href="#">Themes</a>
+                    </li>
+                    <li className="nav-item nav_margin">
+                        <a className="nav-link active" aria-current="page" href="#">Pricing</a>
+                    </li>
+                    <li className="nav-item nav_margin">
+                        <a className="nav-link active" aria-current="page" href="#">Contact</a>
+                    </li>
+                </ul>
+            </div>
+                  
+              </div>
+          </nav>
+      </div>
    </>
   )
 }
--- src/app/Header/Header.js
+++ src/app/Header/Header.js
@@ -4,35 +4,25 @@ import vosovyapar from '../../../public/images/voso.svg';
 import "./Header.css";
 export const Header = () => {
   return (
-      <div>
-        
-          <nav className="navbar navbar-expand-lg  mynav">
-              <div className="container-fluid">
-             
-                    <Image src={vosovyapar} className='voso_logo' />
-                  
-                 <div>
-                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li className="nav-item nav_margin">
-                         <a className="nav-link active " aria-current="page" href="#">About Us</a>
-                    </li>
-                    <li className="nav-item nav_margin">
-                        <a className="nav-link active" aria-current="page" href="#">How It Work</a>
-                    </li>
-                    <li className="nav-item nav_margin">
-                        <a className="nav-link active" aria-current="page" href="#">Themes</a>
-                    </li>
-                    <li className="nav-item nav_margin">
-                        <a className="nav-link active" aria-current="page" href="#">Pricing</a>
-                    </li>
-                    <li className="nav-item nav_margin">
-                        <a className="nav-link active" aria-current="page" href="#">Contact</a>
-                    </li>
-                </ul>
-            </div>
-                  
-              </div>
-          </nav>
+    <nav className="navbar navbar-expand-lg bg-body-tertiary  mynav">
+    <div className="container-fluid">
+    <Image src={vosovyapar} className='voso_logo' />
+      
+      <div className="collapse navbar-collapse" id="navbarText">
+        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          <li className="nav-item">
+            <a className="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Features</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Pricing</a>
+          </li>
+        </ul>
+       
       </div>
+    </div>
+  </nav>
   )
 }
